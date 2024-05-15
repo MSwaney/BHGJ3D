291a637c35f3222237b686034a5b351666ca9a97
Matthew Swaney <Phiktional@Gmail.com>
2024-05-14 19:33:47 -0700

    reestablishing baseline

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
+    [Header ("Player Stats")]
     [SerializeField] private float _moveSpeed;
-    [SerializeField] BulletSpawner _bulletSpawner;
+    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _damage;
+
+    [Header ("Movement Settings")]
     [SerializeField] private float _dodgeSpeedMultiplier;
     [SerializeField] private float _dodgeDuration;
-    [SerializeField] private Animator _animator;
     [SerializeField] private float _rotationSpeed;
+    [SerializeField] private int _dodgeDamage;
 
-    private Vector2 _moveInput;
-    private Vector3 _moveDirection;
+    [Header("Laser Settings")]
+    [SerializeField] private bool _canFireLaser;
+    [SerializeField] private float _laserSpeed;
+    [SerializeField] private float _laserCooldown;
+
+    [Header ("Everything Else")]
+    [SerializeField] private Animator _animator;
+    [SerializeField] private BulletSpawner _bulletSpawner;
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private Rigidbody _rigidbody;
+
     private bool _isDodging;
     private bool _isMoving;
+    private bool _isMovingBackwards;
+    private Vector2 _moveInput;
+    private Vector3 _moveDirection;
 
     public bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _health = _maxHealth;
     }
 
     // Update is called once per frame
@@ -33,6 +50,14 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && !_isDodging)
         {
             StartCoroutine(StartDodge());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_canFireLaser && Input.GetMouseButton(0))
+        {
+            FireLaser();
         }
     }
 
@@ -55,15 +80,62 @@ public class PlayerController : MonoBehaviour
             _animator.SetBool("isMoving", _isMoving);
         }
 
-        // Calculate movement direction
-        _moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+        // Calculate movement direction based on input
+        Vector3 inputDirection = new Vector3(_moveInput.x, 0f, _moveInput.y);
 
-        // Rotate the character to face the movement direction
-        if (_moveDirection.magnitude > 0.1f) // Only rotate if there's significant movement
+        // Calculate dot product with character's forward direction
+        float forwardDirection = Vector3.Dot(transform.forward, inputDirection.normalized);
+
+        // Set Animator parameters based on dot product
+        if (forwardDirection > 0f)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+            _isMoving = true;
+            _isMovingBackwards = false;
         }
+        else if (forwardDirection < 0f)
+        {
+            _isMoving = false;
+            _isMovingBackwards = true;
+        }
+        else
+        {
+            _isMoving = false;
+            _isMovingBackwards = false;
+        }
+
+        print(_isMoving + ", " +  _isMovingBackwards);
+        _animator.SetBool("isMoving", _isMoving);
+        _animator.SetBool("isMovingBackwards", _isMovingBackwards);
+
+        // Cast a ray from the mouse position
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        // Check if the ray intersects with the ground plane
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            // Get the point of intersection
+            Vector3 targetPoint = ray.GetPoint(rayDistance);
+
+            // Ensure the target point is at the same height as the player
+            targetPoint.y = transform.position.y;
+
+            // Rotate the player towards the target point
+            RotateTowardsCursor(targetPoint);
+        }
+    }
+
+    private void RotateTowardsCursor(Vector3 targetPoint)
+    {
+        //Calculate the direction to the target point
+        Vector3 direction = targetPoint - transform.position;
+
+        //Calculate the rotation to face the target point
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        //Smoothly rotate the player towards the target point
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
     }
 
     private IEnumerator StartDodge()
@@ -71,6 +143,7 @@ public class PlayerController : MonoBehaviour
         _isDodging = true;
         float currentSpeed = _moveSpeed;
         _moveSpeed *= _dodgeSpeedMultiplier;
+        _health -= _dodgeDamage;
 
         //Wait for the duration of the dodge
         yield return new WaitForSeconds(_dodgeDuration);
@@ -78,6 +151,22 @@ public class PlayerController : MonoBehaviour
         //Reset dodge state and speed
         _isDodging = false;
         _moveSpeed = currentSpeed;
+    }
+
+    private void FireLaser()
+    {
+        Vector3 laserOffset = transform.forward * 1.05f;
+        GameObject laser = Instantiate(_laserPrefab, transform.position + laserOffset, Quaternion.identity);
+        laser.GetComponent<Rigidbody>().velocity = transform.forward * _laserSpeed;
+
+        StartCoroutine(LaserCooldown());
+    }
+
+    private IEnumerator LaserCooldown()
+    {
+        _canFireLaser = false;
+        yield return new WaitForSeconds(_laserCooldown);
+        _canFireLaser = true;
     }
 
     public void PlayerDied()
